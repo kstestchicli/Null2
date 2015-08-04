53eb9a2259b9622d00bc31eba84adeb5a5976554
kstestchicli <kstestchili.com+1@gmail.com>
2015-08-03 22:22:12 -0700

    commit to master branch
    this is a test for null repository

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            //test application.new test
+            //test application.new test.Now there is a change.
         }
     }
 }
